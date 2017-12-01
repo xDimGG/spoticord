86577d692bee2c0f70d796cdf5a1944fea28bd80
Dim <dimggyt@gmail.com>
2017-11-30 20:49:35 -0500

    woops

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @echo off
 
-if not exist "%~dp0/../node_modules" (
+if not exist "%~dp0/node_modules" (
 	echo Installing Dependencies
 	echo Run this file again after it closes
 	npm i
@@ -11,5 +11,5 @@ cls
 echo Launching app.js (Don't worry about warnings)
 echo.
 :loop
-node "%~dp0/../app.js"
+node "%~dp0/app.js"
 goto loop
